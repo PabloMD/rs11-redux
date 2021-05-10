7b6ae50fbb71d8cf94bfaf13af5c41a1b2f8799d
PabloMD <chmielecki7@gmail.com>
2021-05-10 23:01:07 +0200

    quick refactor

--- src/components/Message/MessagesList.js
+++ src/components/Message/MessagesList.js
@@ -3,9 +3,8 @@ import Message from "./Message";
 function MessagesList({ messages }){
     if(messages && messages.length){
     return <div>
-        { messages.map((message, i) => (
+        { messages.map((message) => (
             <Message key={`message-${message.id}`} id={message.id} message={message.message} type={message.type} />
-
         ))}
     </div>;
     }
